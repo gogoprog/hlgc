063de6b2d4b053056802fedeaaeefa2d4548062e
Gauthier Billot <gogoprog@gmail.com>
2024-01-09 22:03:09 +0100

    wip

--- Main.hx
+++ Main.hx
@@ -1,9 +1,48 @@
 package;
 
+class Obj {
+    public var value:Int;
+    public function new() {
+        value = Std.random(10);
+    }
+
+    var dummy = [];
+    var dummy2 = [];
+    var dummy3 = [];
+    var dummy4 = [];
+    var dummy5 = [];
+    var dummy6 = [];
+    var dummy7 = [];
+    var dummy8 = [];
+    var dummy9 = [];
+}
+
 class Main {
 
     static function main() {
         trace("Hello");
+
+        for(n in 0...1000000) {
+            var a:Array<Obj> = [];
+
+
+            for(k in 0...10000) {
+                var i = Std.random(10000);
+                a[i] = new Obj();
+            }
+
+            var r = a[Std.random(10000)];
+            var exists = (r != null);
+
+            for(k in 0...100) {
+                var j = Std.random(10000);
+                a[j] = null;
+            }
+
+            if(r != null) {
+                trace(r.value);
+            }
+        }
     }
 
 }
--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-all:
+haxe:
 	haxe build.hxml
+
+native: haxe
+	gcc out/main.c -o out/main -L ${HL}/build/bin -lhl -I out/ -I ${HL}/src
+
+run: native
+	LD_LIBRARY_PATH=${HL}/build/bin out/main
+
+
+
